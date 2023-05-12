ce5d0271020f5a7ccfdb4a1c6d40a954879aaa6e
MarceloLaboral <coria.marcelo.90@gmail.com>
2023-05-11 23:55:06 -0300

    Everything up-to-date

--- src/services/daos/models/mongo/users.model.js
+++ src/services/daos/models/mongo/users.model.js
@@ -1,11 +1,22 @@
 import mongoose from 'mongoose'
 
 const usuarioSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-  password: { type: String },
   first_name: { type: String },
   last_name: { type: String },
+  email: { type: String, required: true, unique: true },
   age: { type: Number },
+  password: { type: String },
+  carts: {
+    type: [
+      {
+        _id: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'carts'
+        }
+      }
+    ],
+    required: true
+  },
   rol: { type: String, enum: ['user', 'admin'], default: 'user' }
 }, { versionKey: false })
 
